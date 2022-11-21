5b7f10cd0274da3c7f39d42acf3bf1c3d02df757
Alex <112178448+Alx424@users.noreply.github.com>
2022-11-21 19:54:32 +0000

    c

--- sketch.js
+++ sketch.js
@@ -4,7 +4,16 @@ class NeuralNetwork {
 
 
     constructor(input_nodes, hidden_nodes, output_nodes){
-        
+
+
+
+        this.input_nodes = input_nodes;
+        this.hidden_nodes = hidden_nodes;
+        this.output.nodes = output.nodes;
+
+        //initialize random weights
+        this.input_weights = tf.randomNormal([this.input_nodes, this.hidden_nodes])
+        this.output_weights = tf.randomNormal([this.hidden_nodes, this.output_nodes])
     }
 
 
